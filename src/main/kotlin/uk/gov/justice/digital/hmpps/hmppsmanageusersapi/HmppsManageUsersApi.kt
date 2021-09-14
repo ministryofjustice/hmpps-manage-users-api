56108d055349cf43eb5e933c5b1cf2e197f7573a
SimonMitchellMOJ <SimonMitchellMOJ@users.noreply.github.com>
2021-09-14 08:52:56 +0000

    updating project name after deployment from template repository

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageusersapi/HmppsManageUsersApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageusersapi/HmppsManageUsersApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.hmppstemplatepackagename
+package uk.gov.justice.digital.hmpps.hmppsmanageusersapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication()
-class HmppsTemplateKotlin
+class HmppsManageUsersApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<HmppsManageUsersApi>(*args)
 }
