fa53a48e6f5e7b10efdf2b6002a5045db9f8a378
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2021-09-24 11:25:19 +0100

    DT-2594 Add ability to change role name (#6)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/service/AuthService.kt
@@ -0,0 +1,50 @@
+package uk.gov.justice.digital.hmpps.manageusersapi.service
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import uk.gov.justice.digital.hmpps.manageusersapi.resource.Role
+import uk.gov.justice.digital.hmpps.manageusersapi.resource.RoleNameAmendment
+
+@Service
+class AuthService(
+  val authWebClient: WebClient,
+) {
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  @Throws(RoleNotFoundException::class)
+  fun getRoleDetail(roleCode: String): Role {
+    try {
+      return authWebClient.get()
+        .uri("/api/roles/$roleCode")
+        .retrieve()
+        .bodyToMono(Role::class.java)
+        .block() ?: throw RoleNotFoundException("get", roleCode, "notfound")
+    } catch (e: WebClientResponseException) {
+      throw if (e.statusCode.equals(HttpStatus.NOT_FOUND)) RoleNotFoundException("get", roleCode, "notfound") else e
+    }
+  }
+
+  @Throws(RoleNotFoundException::class)
+  fun updateRoleName(roleCode: String, roleAmendment: RoleNameAmendment) {
+    log.debug("Updating role for $roleCode with {}", roleAmendment)
+    try {
+      authWebClient.put()
+        .uri("/api/roles/$roleCode")
+        .bodyValue(roleAmendment)
+        .retrieve()
+        .toBodilessEntity()
+        .block()
+    } catch (e: WebClientResponseException) {
+      throw if (e.statusCode.equals(HttpStatus.NOT_FOUND)) RoleNotFoundException("get", roleCode, "notfound") else e
+    }
+  }
+}
+
+class RoleNotFoundException(action: String, role: String, errorCode: String) :
+  Exception("Unable to $action role: $role with reason: $errorCode")
--- src/test/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/resource/RolesControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/resource/RolesControllerIntTest.kt
@@ -1,46 +1,53 @@
 package uk.gov.justice.digital.hmpps.manageusersapi.resource
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.BodyInserters
 import uk.gov.justice.digital.hmpps.manageusersapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.manageusersapi.integration.wiremock.HmppsAuthApiExtension.Companion.hmppsAuth
 
 class RolesControllerIntTest : IntegrationTestBase() {
 
-  @Test
-  fun `access forbidden when no authority`() {
+  @Nested
+  inner class RoleDetails {
 
-    webTestClient.get().uri("/roles/role-code")
-      .exchange()
-      .expectStatus().isUnauthorized
-  }
+    @Test
+    fun `access forbidden when no authority`() {
 
-  @Test
-  fun `access forbidden when no role`() {
+      webTestClient.get().uri("/roles/role-code")
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
 
-    webTestClient.get().uri("/roles/AUTH_GROUP_MANAGER")
-      .headers(setAuthorisation(roles = listOf()))
-      .exchange()
-      .expectStatus().isForbidden
-  }
+    @Test
+    fun `access forbidden when no role`() {
 
-  @Test
-  fun `access forbidden when wrong role`() {
+      webTestClient.get().uri("/roles/AUTH_GROUP_MANAGER")
+        .headers(setAuthorisation(roles = listOf()))
+        .exchange()
+        .expectStatus().isForbidden
+    }
 
-    webTestClient.get().uri("/roles/AUTH_GROUP_MANAGER")
-      .headers(setAuthorisation(roles = listOf("ROLE_AUDIT")))
-      .exchange()
-      .expectStatus().isForbidden
-  }
+    @Test
+    fun `access forbidden when wrong role`() {
 
-  @Test
-  fun `get role`() {
-    hmppsAuth.stubGetRolesDetails()
-    webTestClient.get().uri("/roles/AUTH_GROUP_MANAGER")
-      .headers(setAuthorisation(roles = listOf("ROLE_ROLES_ADMIN")))
-      .exchange()
-      .expectStatus().isOk
-      .expectBody().json(
-        """
+      webTestClient.get().uri("/roles/AUTH_GROUP_MANAGER")
+        .headers(setAuthorisation(roles = listOf("ROLE_AUDIT")))
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `get role`() {
+      hmppsAuth.stubGetRolesDetails()
+      webTestClient.get().uri("/roles/AUTH_GROUP_MANAGER")
+        .headers(setAuthorisation(roles = listOf("ROLE_ROLES_ADMIN")))
+        .exchange()
+        .expectStatus().isOk
+        .expectBody().json(
+          """
           {
           "roleCode":"AUTH_GROUP_MANAGER",
           "roleName":"Group Manager",
@@ -51,6 +58,138 @@ class RolesControllerIntTest : IntegrationTestBase() {
             "adminTypeName":"External Administrator"}]
           }
           """
-      )
+        )
+    }
+  }
+
+  @Nested
+  inner class AmendRoleName {
+
+    @Test
+    fun `Change role name endpoint not accessible without valid token`() {
+      webTestClient.put().uri("/roles/ANY_ROLE")
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
+
+    @Test
+    fun `Change role name endpoint returns forbidden when does not have admin role `() {
+      webTestClient
+        .put().uri("/roles/ANY_ROLE")
+        .headers(setAuthorisation(roles = listOf()))
+        .body(BodyInserters.fromValue(mapOf("roleName" to "new role name")))
+        .exchange()
+        .expectStatus().isForbidden
+        .expectBody()
+        .jsonPath("$").value<Map<String, Any>> {
+          mapOf(
+            "status" to "403"
+          )
+        }
+    }
+
+    @Test
+    fun `Change role name returns error when role not found`() {
+      hmppsAuth.stubPutRoleNameFail("Not_A_Role", 404)
+      webTestClient
+        .put().uri("/roles/Not_A_Role")
+        .headers(setAuthorisation(roles = listOf("ROLE_ROLES_ADMIN")))
+        .body(BodyInserters.fromValue(mapOf("roleName" to "new role name")))
+        .exchange()
+        .expectStatus().isNotFound
+        .expectHeader().contentType(MediaType.APPLICATION_JSON)
+        .expectBody()
+        .jsonPath("status").isEqualTo("404")
+        .jsonPath("$").value<Map<String, Any>> {
+          assertThat(it["userMessage"] as String).isEqualTo("Unexpected error: Unable to get role: Not_A_Role with reason: notfound")
+          assertThat(it["developerMessage"] as String).isEqualTo("Unable to get role: Not_A_Role with reason: notfound")
+        }
+    }
+
+    @Test
+    fun `Change role name returns error when length too short`() {
+      webTestClient
+        .put().uri("/roles/OAUTH_ADMIN")
+        .headers(setAuthorisation(roles = listOf("ROLE_ROLES_ADMIN")))
+        .body(BodyInserters.fromValue(mapOf("roleName" to "tim")))
+        .exchange()
+        .expectStatus().isBadRequest
+        .expectBody()
+        .jsonPath("$").value<Map<String, Any>> {
+          assertThat(it["userMessage"] as String).isEqualTo("Validation failure: roleNameAmendment")
+          assertThat(it["developerMessage"] as String).contains("default message [roleName],100,4]")
+        }
+    }
+
+    @Test
+    fun `Change role name returns error when length too long`() {
+      webTestClient
+        .put().uri("/roles/OAUTH_ADMIN")
+        .headers(setAuthorisation(roles = listOf("ROLE_ROLES_ADMIN")))
+        .body(
+          BodyInserters.fromValue(
+            mapOf(
+              "roleName" to "12345".repeat(20) + "y",
+            )
+          )
+        )
+        .exchange()
+        .expectStatus().isBadRequest
+        .expectBody()
+        .jsonPath("$").value<Map<String, Any>> {
+          assertThat(it["userMessage"] as String).isEqualTo("Validation failure: roleNameAmendment")
+          assertThat(it["developerMessage"] as String).contains("default message [roleName],100,4]")
+        }
+    }
+
+    @Test
+    fun `Change role name failed regex`() {
+      webTestClient
+        .put().uri("/roles/OAUTH_ADMIN")
+        .headers(setAuthorisation(roles = listOf("ROLE_ROLES_ADMIN")))
+        .body(
+          BodyInserters.fromValue(
+            mapOf(
+              "roleName" to "a\$here",
+            )
+          )
+        )
+        .exchange()
+        .expectStatus().isBadRequest
+        .expectBody()
+        .jsonPath("$").value<Map<String, Any>> {
+          assertThat(it["userMessage"] as String).isEqualTo("Validation failure: roleNameAmendment")
+          assertThat(it["developerMessage"] as String).contains("[roleName]")
+          assertThat(it["developerMessage"] as String).contains("[must match \"^[0-9A-Za-z- ,.()'&]*\$\"]]")
+        }
+    }
+
+    @Test
+    fun `Change role name success`() {
+      hmppsAuth.stubPutRoleName("OAUTH_ADMIN")
+      webTestClient
+        .put().uri("/roles/OAUTH_ADMIN")
+        .headers(setAuthorisation(roles = listOf("ROLE_ROLES_ADMIN")))
+        .body(BodyInserters.fromValue(mapOf("roleName" to "new role name")))
+        .exchange()
+        .expectStatus().isOk
+    }
+
+    @Test
+    fun `Change role name passes regex validation`() {
+      hmppsAuth.stubPutRoleName("OAUTH_ADMIN")
+      webTestClient
+        .put().uri("/roles/OAUTH_ADMIN")
+        .headers(setAuthorisation(roles = listOf("ROLE_ROLES_ADMIN")))
+        .body(
+          BodyInserters.fromValue(
+            mapOf(
+              "roleName" to "good's & Role(),.-"
+            )
+          )
+        )
+        .exchange()
+        .expectStatus().isOk
+    }
   }
 }
