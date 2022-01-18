52311a20b942b743d9d5f87377485cf1b2a65bf4
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-01-18 09:41:19 +0000

    DT-2498 Separate generic methods into superclass

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/service/RoleSyncService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/service/RoleSyncService.kt
@@ -1,9 +1,6 @@
 package uk.gov.justice.digital.hmpps.manageusersapi.service
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import com.google.common.collect.MapDifference
-import com.google.common.collect.Maps
-import com.google.common.reflect.TypeToken
 import com.google.gson.Gson
 import com.microsoft.applicationinsights.TelemetryClient
 import io.swagger.v3.oas.annotations.media.Schema
@@ -13,34 +10,34 @@ import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.manageusersapi.resource.Role
 import uk.gov.justice.digital.hmpps.manageusersapi.service.AdminType.DPS_ADM
 import uk.gov.justice.digital.hmpps.manageusersapi.service.AdminType.DPS_LSA
-import uk.gov.justice.digital.hmpps.manageusersapi.service.RoleDifferences.UpdateType.ERROR
-import uk.gov.justice.digital.hmpps.manageusersapi.service.RoleDifferences.UpdateType.INSERT
-import uk.gov.justice.digital.hmpps.manageusersapi.service.RoleDifferences.UpdateType.NONE
-import uk.gov.justice.digital.hmpps.manageusersapi.service.RoleDifferences.UpdateType.UPDATE
+import uk.gov.justice.digital.hmpps.manageusersapi.service.SyncDifferences.UpdateType.ERROR
+import uk.gov.justice.digital.hmpps.manageusersapi.service.SyncDifferences.UpdateType.INSERT
+import uk.gov.justice.digital.hmpps.manageusersapi.service.SyncDifferences.UpdateType.NONE
+import uk.gov.justice.digital.hmpps.manageusersapi.service.SyncDifferences.UpdateType.UPDATE
 
 @Service
 class RoleSyncService(
   private val nomisApiService: NomisApiService,
   private val authService: AuthService,
   private val telemetryClient: TelemetryClient,
-  private val gson: Gson
-) {
+  gson: Gson
+) : SyncService(gson) {
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
 
   fun sync(readOnly: Boolean = true): SyncStatistics {
-    return syncAllRoles(authService.getRoles(listOf(DPS_ADM)), nomisApiService.getAllRoles(), readOnly)
+    return syncAllData(authService.getRoles(listOf(DPS_ADM)), nomisApiService.getAllRoles(), readOnly)
   }
 
-  private fun syncAllRoles(
+  private fun syncAllData(
     rolesFromAuth: List<Role>,
     rolesFromNomis: List<NomisRole>,
     readOnly: Boolean
   ): SyncStatistics {
 
-    val rolesFromNomisMap = rolesFromNomis.map { RoleDataToSync(it) }.associateBy { it.roleCode }
     val rolesFromAuthMap = rolesFromAuth.map { RoleDataToSync(it) }.associateBy { it.roleCode }
+    val rolesFromNomisMap = rolesFromNomis.map { RoleDataToSync(it) }.associateBy { it.roleCode }
 
     val stats = SyncStatistics()
     rolesFromAuthMap.filterMatching(rolesFromNomisMap).forEach {
@@ -53,14 +50,6 @@ class RoleSyncService(
       syncRole(rolesFromNomisMap[it.key], null, stats, readOnly)
     }
     return stats
-  }
-
-  private fun <K, V> Map<out K, V>.filterNew(rolesMap: Map<K, V>): Map<K, V> = filter { r -> rolesMap[r.key] == null }
-  private fun <K, V> Map<out K, V>.filterMatching(rolesMap: Map<K, V>): Map<K, V> =
-    filter { r -> rolesMap[r.key] != null }
-
-  private fun <K, V> Map<out K, RoleDataToSync>.filterMissing(rolesMap: Map<K, V>): Map<K, RoleDataToSync> {
-    return filter { r -> rolesMap[r.key] == null }
   }
 
   fun syncRole(currentRoleData: RoleDataToSync?, newRoleData: RoleDataToSync?, stats: SyncStatistics, readOnly: Boolean) {
@@ -77,15 +66,15 @@ class RoleSyncService(
       } else {
         try {
           storeInNomis(currentRoleData, newRoleData!!, stats)
-          if (stats.roles[newRoleData.roleCode]?.updateType != NONE) {
+          if (stats.results[newRoleData.roleCode]?.updateType != NONE) {
             val trackingAttributes = mapOf(
               "roleCode" to newRoleData.roleCode,
-              "differences" to stats.roles[newRoleData.roleCode]?.differences,
+              "differences" to stats.results[newRoleData.roleCode]?.differences,
             )
             telemetryClient.trackEvent("HMUA-Role-Change", trackingAttributes, null)
           }
         } catch (e: Exception) {
-          stats.roles[newRoleData!!.roleCode] = stats.roles[newRoleData.roleCode]!!.copy(updateType = ERROR)
+          stats.results[newRoleData!!.roleCode] = stats.results[newRoleData.roleCode]!!.copy(updateType = ERROR)
 
           log.error("Failed to update {} - message = {}", newRoleData.roleCode, e.message)
           telemetryClient.trackEvent("HMUA-Role-Change-Failure", mapOf("roleCode" to newRoleData.roleCode), null)
@@ -96,9 +85,9 @@ class RoleSyncService(
 
   private fun MapDifference<String, Any>.storeStatistics(currentRoleData: RoleDataToSync?, newRoleData: RoleDataToSync?, stats: SyncStatistics) {
     if (entriesOnlyOnLeft().isNotEmpty()) {
-      stats.roles[currentRoleData!!.roleCode] = RoleDifferences(currentRoleData.roleCode, toString())
+      stats.results[currentRoleData!!.roleCode] = SyncDifferences(currentRoleData.roleCode, toString())
     } else
-      stats.roles[newRoleData!!.roleCode] = RoleDifferences(newRoleData.roleCode, toString())
+      stats.results[newRoleData!!.roleCode] = SyncDifferences(newRoleData.roleCode, toString())
   }
 
   private fun storeInNomis(
@@ -109,34 +98,17 @@ class RoleSyncService(
     currentRoleData?.let {
       if (newRoleData != currentRoleData) { // don't update if equal
         nomisApiService.updateRole(it.roleCode, newRoleData.roleName, newRoleData.adminRoleOnly)
-        stats.roles[newRoleData.roleCode] = stats.roles[newRoleData.roleCode]!!.copy(updateType = UPDATE)
+        stats.results[newRoleData.roleCode] = stats.results[newRoleData.roleCode]!!.copy(updateType = UPDATE)
       }
     } ?: run {
       // missing from Nomis
       nomisApiService.createRole(NomisRole(newRoleData.roleCode, newRoleData.roleName, newRoleData.adminRoleOnly))
-      stats.roles[newRoleData.roleCode] = stats.roles[newRoleData.roleCode]!!.copy(updateType = INSERT)
+      stats.results[newRoleData.roleCode] = stats.results[newRoleData.roleCode]!!.copy(updateType = INSERT)
     }
 
-    if (stats.roles[newRoleData.roleCode]?.updateType == NONE) {
-      stats.roles.remove(newRoleData.roleCode)
+    if (stats.results[newRoleData.roleCode]?.updateType == NONE) {
+      stats.results.remove(newRoleData.roleCode)
     }
-  }
-
-  fun checkForDifferences(
-    existingRecord: RoleDataToSync?,
-    newRecord: RoleDataToSync?
-  ): MapDifference<String, Any> {
-    val leftMap = existingRecord?.let { it.asMap() } ?: mapOf()
-    val rightMap = newRecord?.let { it.asMap() } ?: mapOf()
-    return Maps.difference(leftMap, rightMap)
-  }
-
-  private fun RoleDataToSync.asMap(): Map<String, Any> {
-    return (
-      gson.fromJson(
-        gson.toJson(this), object : TypeToken<Map<String, Any>>() {}.type
-      )
-      )
   }
 }
 
@@ -170,17 +142,17 @@ data class RoleDataToSync(
       roleFromNomis.adminRoleOnly
     )
 }
-
+/*
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Schema(description = "Sync Statistics")
 data class SyncStatistics(
   @Schema(description = "Map of all roles have have been inserted, updated or errored")
-  val roles: MutableMap<String, RoleDifferences> = mutableMapOf()
+  val results: MutableMap<String, SyncDifferences> = mutableMapOf()
 )
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Schema(description = "Role Changes")
-data class RoleDifferences(
+data class SyncDifferences(
   @Schema(description = "The code of the Role", example = "GLOBAL_SEARCH") val roleCode: String,
   @Schema(description = "Differences listed", example = "Global Searcher") val differences: String,
   @Schema(description = "Type of update", example = "INSERT") val updateType: UpdateType = NONE
@@ -189,3 +161,4 @@ data class RoleDifferences(
     NONE, INSERT, UPDATE, ERROR
   }
 }
+*/
