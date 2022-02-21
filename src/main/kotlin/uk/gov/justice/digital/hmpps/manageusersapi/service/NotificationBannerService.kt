74dda8d4366833dbe3a7df143d680002df90eccd
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2022-02-21 15:59:40 +0000

    HAAR-106: add logging to FileNotFoundException

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/service/NotificationBannerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/service/NotificationBannerService.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.manageusersapi.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.util.ResourceUtils.getFile
 import uk.gov.justice.digital.hmpps.manageusersapi.resource.NotificationMessage
@@ -8,10 +9,15 @@ import java.io.FileNotFoundException
 @Service
 class NotificationBannerService {
 
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
   fun getNotificationMessage(type: String, env: String): NotificationMessage {
     val message: String = try {
       getFile("classpath:${env}Banners/${type}BannerMessage.txt").readText(Charsets.UTF_8)
     } catch (e: FileNotFoundException) {
+      log.debug("classpath:${env}Banners/${type}BannerMessage.txt")
       ""
     }
 
