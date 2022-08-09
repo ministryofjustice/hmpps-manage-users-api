ba7435e0686fbfa1439d351f37d642efa07f35b1
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-08-09 15:28:26 +0100

    HAAR-697 upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,9 @@ dependencies {
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
-  implementation("com.google.code.gson:gson:2.9.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.3")
+  implementation("com.google.code.gson:gson:2.9.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
   implementation("uk.gov.service.notify:notifications-java-client:3.17.3-RELEASE")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
@@ -29,7 +29,7 @@ dependencies {
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.mockito:mockito-inline:4.6.1")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.3")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
 
 java {
