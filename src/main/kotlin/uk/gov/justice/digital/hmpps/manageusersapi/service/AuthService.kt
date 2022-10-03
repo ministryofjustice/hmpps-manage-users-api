68150e9093eeaf1a72ab58cb257f7536e0431df9
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2022-10-03 09:20:39 +0100

    HAAR-832: removed obsolete function from AuthService

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/service/AuthService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/service/AuthService.kt
@@ -4,12 +4,10 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.awaitBody
-import uk.gov.justice.digital.hmpps.manageusersapi.resource.RoleDescriptionAmendment
 import java.time.Duration
 
 @Service
@@ -21,21 +19,6 @@ class AuthService(
 ) {
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
-  }
-
-  @Throws(RoleNotFoundException::class)
-  fun updateRoleDescription(roleCode: String, roleAmendment: RoleDescriptionAmendment) {
-    log.debug("Updating role for {} with {}", roleCode, roleAmendment)
-    try {
-      authWebClient.put()
-        .uri("/api/roles/$roleCode/description")
-        .bodyValue(roleAmendment)
-        .retrieve()
-        .toBodilessEntity()
-        .block(timeout)
-    } catch (e: WebClientResponseException) {
-      throw if (e.statusCode.equals(HttpStatus.NOT_FOUND)) RoleNotFoundException("get", roleCode, "notfound") else e
-    }
   }
 
   @Throws(TokenException::class)
