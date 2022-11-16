e114f3fe0d54198cc47f053404e9ccf558e9694b
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-11-16 11:16:26 +0000

    HAAR-994: ⬆️ upgrade gradle spring boot plugin to 4.6.0-beta (#166)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.7"
-  kotlin("plugin.spring") version "1.7.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.6.0-beta"
+  kotlin("plugin.spring") version "1.7.21"
 }
 
 configurations {
@@ -15,8 +15,8 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-ui:1.6.12")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.12")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.0")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
   implementation("com.google.code.gson:gson:2.10")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
@@ -28,7 +28,7 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.mockito:mockito-inline:4.8.1")
+  testImplementation("org.mockito:mockito-inline:4.9.0")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
 
