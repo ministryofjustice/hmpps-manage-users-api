93b4126b870a67ae5c444996bb531ea3bb41664c
karenmillermoj <karen.miller@digital.justice.gov.uk>
2023-01-17 15:56:36 +0000

    HAAR-884 Add UserDetailsDto to match type returned from auth findUserByUsername endpoint

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/model/UserDetailsDto.kt
@@ -0,0 +1,36 @@
+package uk.gov.justice.digital.hmpps.manageusersapi.model
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
+import io.swagger.v3.oas.annotations.media.Schema
+import java.util.UUID
+
+@JsonInclude(NON_NULL)
+@Schema(description = "User Details")
+data class UserDetailsDto(
+  @Schema(description = "Username", example = "DEMO_USER1")
+  val username: String,
+
+  @Schema(description = "Active", example = "false")
+  var active: Boolean,
+
+  @Schema(description = "Name", example = "John Smith")
+  var name: String,
+
+  @Schema(title = "Authentication Source", description = "auth for external users, nomis for nomis authenticated users", example = "nomis")
+  var authSource: AuthSource,
+
+  @Schema(title = "User Id", description = "Unique identifier for user, will be UUID for external users or staff ID for nomis users", example = "231232")
+  var userId: String,
+
+  @Schema(title = "Unique Id", description = "Universally unique identifier for user, generated and stored in auth database for all users", example = "5105a589-75b3-4ca0-9433-b96228c1c8f3")
+  var uuid: UUID? = null,
+
+  @Deprecated("")
+  @Schema(title = "Staff Id", description = "Deprecated, use userId instead", example = "231232")
+  var staffId: Long? = null,
+
+  @Deprecated("")
+  @Schema(title = "Current Active Caseload", description = "Deprecated, retrieve from prison API rather than manage users", example = "MDI")
+  var activeCaseLoadId: String? = null
+)
