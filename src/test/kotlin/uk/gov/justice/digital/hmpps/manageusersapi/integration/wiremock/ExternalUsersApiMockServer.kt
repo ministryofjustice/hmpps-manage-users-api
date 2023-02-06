70dd875e98553e1cb08c9870eea08dea9a60ef24
ravmoj <104509282+ravmoj@users.noreply.github.com>
2023-02-06 13:30:53 +0000

    HAAR-868: Get searchable roles (#209)
    * HAAR-868: Get list of searchable roles
    
    * HAAR-868: Get list of searchable roles
    
    * HAAR-868: Get list of searchable roles
    
    * HAAR-868: upadted method name

--- src/test/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/integration/wiremock/ExternalUsersApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/integration/wiremock/ExternalUsersApiMockServer.kt
@@ -2044,4 +2044,26 @@ class ExternalUsersApiMockServer : WireMockServer(WIREMOCK_PORT) {
         )
     )
   }
+
+  fun stubGetSearchableRoles() {
+    stubFor(
+      get(urlEqualTo("/users/me/searchable-roles"))
+        .willReturn(
+          aResponse()
+            .withHeaders(HttpHeaders(HttpHeader("Content-Type", "application/json")))
+            .withBody(
+              """
+                [
+                  {
+                    "roleCode": "PF_POLICE",
+                    "roleName": "Pathfinder Police",
+                    "roleDescription": null
+                  }
+                ]
+                  
+              """.trimIndent()
+            )
+        )
+    )
+  }
 }
