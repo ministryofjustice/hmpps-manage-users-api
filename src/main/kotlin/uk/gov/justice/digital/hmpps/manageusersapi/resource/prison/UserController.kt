3e45cf469daa8d1180fa65a6386fa7a1cdab117d
olatomoloju <ola.tomoloju@digital.justice.gov.uk>
2023-04-18 09:12:03 +0100

    HAAR-1257: dto update

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/resource/prison/UserController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/resource/prison/UserController.kt
@@ -549,7 +549,7 @@ data class PrisonStaffUserDto(
   @Schema(description = "Staff ID", example = "324323", required = true) val staffId: Long,
   @Schema(description = "First name of the user", example = "John", required = true) val firstName: String,
   @Schema(description = "Last name of the user", example = "Smith", required = true) val lastName: String,
-  @Schema(description = "Status of staff account", example = "Smith", required = true) val status: String,
+  @Schema(description = "Status of staff account", example = "ACTIVE", required = true) val status: String,
   @Schema(description = "Email addresses of staff", example = "test@test.com", required = false) val primaryEmail: String?,
   @Schema(description = "General user account for this staff member", required = false) val generalAccount: UserCaseloadDto?,
   @Schema(description = "Admin user account for this staff member", required = false) val adminAccount: UserCaseloadDto?,
