39b3cae76a961402116e77564dceb8ee5ffe4699
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2023-05-23 17:02:50 +0100

    HAAR-1402 Reduce pods to 2 in preprod (#277)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.4-beta-4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.4"
   kotlin("plugin.spring") version "1.8.21"
 }
 
@@ -7,10 +7,6 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
-repositories {
-  maven { url = uri("https://repo.spring.io/milestone") }
-  mavenCentral()
-}
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
@@ -19,14 +15,14 @@ dependencies {
 
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.7.0")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.1")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.1")
   implementation("org.apache.commons:commons-text:1.10.0")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
   implementation("com.google.code.gson:gson:2.10.1")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
-  implementation("uk.gov.service.notify:notifications-java-client:3.19.1-RELEASE")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.1")
+  implementation("uk.gov.service.notify:notifications-java-client:4.1.0-RELEASE")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
@@ -34,9 +30,9 @@ dependencies {
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
 
-  testImplementation("io.opentelemetry:opentelemetry-sdk-testing:1.25.0")
+  testImplementation("io.opentelemetry:opentelemetry-sdk-testing:1.26.0")
   testImplementation("javax.xml.bind:jaxb-api:2.3.1")
 }
 
