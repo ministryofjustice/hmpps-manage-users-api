6d60d288bd33a8deaa310762375ab6aa7c64a3c5
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-09-18 12:31:30 +0100

    HAAR-1805: ⬆️ update wiremock dependency (#295)
    * HAAR-1805: ⬆️ update wiremock dependency
    
    * HAAR-1805: ⬆️ update wiremock dependency - 3.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
+  testImplementation("org.wiremock:wiremock:3.0.4")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 
