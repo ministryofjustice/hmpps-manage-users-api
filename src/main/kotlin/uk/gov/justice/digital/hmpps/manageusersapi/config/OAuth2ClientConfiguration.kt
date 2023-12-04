c6954362bbd0c2c09295afef684b8bc011299b64
Jon Brighton <brightonsbox@hotmail.com>
2023-12-04 10:17:14 +0000

    Revert "HAAR-2115: 🐛 Cache client credentials tokens irrespective of principal (#319)"
    This reverts commit ce60aad2018f75fa1fc6e062eb871e31205c0214.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/config/OAuth2ClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/config/OAuth2ClientConfiguration.kt
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.registration.ClientRegistration
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
@@ -35,14 +36,12 @@ class OAuth2ClientConfiguration {
 
   @Bean
   fun authorizedClientManager(
-    clientRegistrationRepository: ClientRegistrationRepository,
-  ): OAuth2AuthorizedClientManager {
+    clientRegistrationRepository: ClientRegistrationRepository?,
+    oAuth2AuthorizedClientService: OAuth2AuthorizedClientService?,
+  ): OAuth2AuthorizedClientManager? {
     val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
     val authorizedClientManager =
-      AuthorizedClientServiceOAuth2AuthorizedClientManager(
-        clientRegistrationRepository,
-        ClientCachingOAuth2AuthorizedClientService(clientRegistrationRepository),
-      )
+      AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, oAuth2AuthorizedClientService)
     authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
     return authorizedClientManager
   }
