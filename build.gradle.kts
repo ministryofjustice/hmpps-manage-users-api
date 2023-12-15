dc7ee00ca480a1ca09ee44f581a2bd7f969129e3
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-12-15 12:14:15 +0000

    HAAR-2154: ⬆️ update to spring-boot-plugin-5.10.1 (#328)
    * HAAR-2154: ⬆️ update to spring-boot-plugin-5.10.1
    
    * HAAR-2154: switch to org.wiremock:wiremock-standalone:3.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.8.0"
-  kotlin("plugin.spring") version "1.9.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.10.1"
+  kotlin("plugin.spring") version "1.9.21"
 }
 
 configurations {
@@ -13,22 +13,22 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.7.0")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.3")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.3")
-  implementation("org.apache.commons:commons-text:1.10.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
+  implementation("org.apache.commons:commons-text:1.11.0")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
   implementation("com.google.code.gson:gson:2.10.1")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.3")
-  implementation("uk.gov.service.notify:notifications-java-client:4.1.0-RELEASE")
+  implementation("uk.gov.service.notify:notifications-java-client:4.1.1-RELEASE")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.3")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.3")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-  testImplementation("org.wiremock:wiremock:3.2.0")
+  testImplementation("org.wiremock:wiremock-standalone:3.3.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 
