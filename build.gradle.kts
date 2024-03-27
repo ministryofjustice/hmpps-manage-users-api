0221aa3d7e2065dd1d73b510ebe6a087a65a45ed
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2024-03-27 17:10:59 +0000

    HAAR-2467: ⬆️ update to spring-boot-plugin-5.15.4 (#345)
    * HAAR-2467: ⬆️ update to spring-boot-plugin-5.15.4
    
    * HAAR-2467: update spring boot 5.15.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
-  kotlin("plugin.spring") version "1.9.22"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.4"
+  kotlin("plugin.spring") version "1.9.23"
 }
 
 configurations {
@@ -13,10 +13,10 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.7.0")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.1")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.8.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
   implementation("org.apache.commons:commons-text:1.11.0")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
   implementation("com.google.code.gson:gson:2.10.1")
@@ -24,7 +24,7 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.0")
   implementation("uk.gov.service.notify:notifications-java-client:5.0.0-RELEASE")
 
-  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
