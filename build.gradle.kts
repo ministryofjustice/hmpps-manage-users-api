2ba005ff19dcbe59c54839eadcb4f424bd670835
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2024-06-24 14:49:57 +0100

    HAAR-2767:  ⬆️ Upgrade to latest spring boot 3.3.0 etc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
-  kotlin("plugin.spring") version "1.9.23"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.0"
+  kotlin("plugin.spring") version "2.0.0"
 }
 
 configurations {
@@ -15,22 +17,22 @@ dependencies {
 
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.8.0")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.0")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.1")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
   implementation("org.apache.commons:commons-text:1.12.0")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
-  implementation("com.google.code.gson:gson:2.10.1")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.0")
-  implementation("uk.gov.service.notify:notifications-java-client:5.0.1-RELEASE")
+  implementation("com.google.code.gson:gson:2.11.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.1")
+  implementation("uk.gov.service.notify:notifications-java-client:5.1.0-RELEASE")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
-  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
-  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-  testImplementation("org.wiremock:wiremock-standalone:3.5.3")
+  testImplementation("org.wiremock:wiremock-standalone:3.7.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
 
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("javax.xml.bind:jaxb-api:2.3.1")
@@ -41,9 +43,9 @@ kotlin {
 }
 
 tasks {
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+  withType<KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "21"
+      compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
     }
   }
 }
