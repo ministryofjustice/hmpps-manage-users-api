eaf4cc85d8529bc4af357b2ad40b4159f96ba06a
Michael Willis <michael.willis@digital.justice.gov.uk>
2025-03-14 16:13:47 +0000

    Update role security requirements in UserCaseloadsController and UserController to include ROLE_USER_PERMISSIONS__RO

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/resource/prison/UserCaseloadsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/resource/prison/UserCaseloadsController.kt
@@ -31,13 +31,13 @@ import uk.gov.justice.digital.hmpps.manageusersapi.model.PrisonCaseload as Priso
 class UserCaseloadsController(
   private val userCaseloadService: UserCaseloadService,
 ) {
-  @PreAuthorize("hasRole('ROLE_MAINTAIN_ACCESS_ROLES_ADMIN') or hasRole('ROLE_MAINTAIN_ACCESS_ROLES') or hasRole('ROLE_USER_PERMISSIONS__RO')")
+  @PreAuthorize("hasAnyRole('ROLE_MAINTAIN_ACCESS_ROLES_ADMIN', 'ROLE_MAINTAIN_ACCESS_ROLES', 'ROLE_USER_PERMISSIONS__RO')")
   @GetMapping("/{username}/caseloads")
   @ResponseStatus(HttpStatus.OK)
   @Operation(
     summary = "Get list of caseloads associated with the users account",
-    description = "Caseloads for a specific user. Requires role ROLE_USER_PERMISSIONS__RO or ROLE_MAINTAIN_ACCESS_ROLES_ADMIN or ROLE_MAINTAIN_ACCESS_ROLES\"",
-    security = [SecurityRequirement(name = "MAINTAIN_ACCESS_ROLES"), SecurityRequirement(name = "MAINTAIN_ACCESS_ROLES_ADMIN")],
+    description = "Caseloads for a specific user. Requires role ROLE_USER_PERMISSIONS__RO or ROLE_MAINTAIN_ACCESS_ROLES_ADMIN or ROLE_MAINTAIN_ACCESS_ROLES",
+    security = [SecurityRequirement(name = "USER_PERMISSIONS__RO"), SecurityRequirement(name = "MAINTAIN_ACCESS_ROLES"), SecurityRequirement(name = "MAINTAIN_ACCESS_ROLES_ADMIN")],
     responses = [
       ApiResponse(
         responseCode = "200",
