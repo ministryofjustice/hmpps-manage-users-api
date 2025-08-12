a5fb150f604998eb61b3f58fcd42f4e7416e49fd
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2025-08-12 09:50:39 +0100

    HAAR-3920: add IMS HIDDEN admin type so consistent with external users and auth

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/model/AdminType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/model/AdminType.kt
@@ -7,6 +7,7 @@ enum class AdminType(val adminTypeCode: String, val adminTypeName: String) {
   DPS_LSA("DPS_LSA", "DPS Local System Administrator"),
   DPS_ADM("DPS_ADM", "DPS Central Administrator"),
   EXT_ADM("EXT_ADM", "External Administrator"),
+  IMS_HIDDEN("IMS_HIDDEN", "IMS Administrator"),
 }
 
 data class AdminTypeReturn(val adminTypeCode: String, val adminTypeName: String)
