88307100b98713fb0fbabf92b418b9d939326a36
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2025-08-19 11:47:19 +0100

    HAAR-4232: remove springdoc-openapi-data-rest and use spring-data-commons to pull in org.springframework.data.domain.Pageable (#410)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/service/external/VerifyEmailDomainService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/service/external/VerifyEmailDomainService.kt
@@ -1,11 +1,9 @@
 package uk.gov.justice.digital.hmpps.manageusersapi.service.external
 
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.manageusersapi.adapter.external.VerifyEmailDomainApiService
 
 @Service
-@Transactional(readOnly = true)
 class VerifyEmailDomainService(val verifyEmailDomainApiService: VerifyEmailDomainApiService) {
 
   fun isValidEmailDomain(emailDomain: String): Boolean = verifyEmailDomainApiService.validateEmailDomain(emailDomain)
