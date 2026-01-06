92eb777d1088b5f0dd40d341fc15d9934c4f0ea9
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2026-01-06 13:30:10 +0000

    HAAR-4805: ✨ Upgrade to java 25 and spring boot plugin 9.3.0 (#435)
    * HAAR-4805: ✨ Upgrade to spring boot plugin 9.3.0
    
    * HAAR-4805: ✨ update owasp action to java 25

--- src/test/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/resource/OpenApiDocsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/manageusersapi/resource/OpenApiDocsTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.manageusersapi.resource
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -53,6 +54,7 @@ class OpenApiDocsTest {
       .accept(MediaType.APPLICATION_JSON)
       .exchange()
       .expectStatus().isOk
-      .expectBody().jsonPath("info.version").isEqualTo(DateTimeFormatter.ISO_DATE.format(LocalDate.now()))
+      .expectBody().jsonPath("info.version")
+      .value { it: Any -> assertThat((it as String).contains(DateTimeFormatter.ISO_DATE.format(LocalDate.now()))) }
   }
 }
